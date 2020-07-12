9e382f964e342316e4e4fcdb90800213f2233e53
d3vk <developerkecil@gmail.com>
2020-07-12 13:33:14 +0700

    Mengubah sumber data

--- covidJateng.py
+++ covidJateng.py
@@ -3,45 +3,35 @@ from datetime import datetime
 import requests
 import re
 
-def getLastUpdate():
+def getData():
     data = []
-    contents = requests.get('https://data.covid19.go.id/public/api/prov_detail_JAWA_TENGAH.json').json()
-    last_update = datetime.strptime(contents['last_date'], "%Y-%m-%d")
-    converted = last_update.strftime("%d-%m-%Y")
-    data.append(converted)
-    case = str(contents['list_perkembangan'][-1]['KASUS'])
-    dead = str(contents['list_perkembangan'][-1]['MENINGGAL'])
-    cured = str(contents['list_perkembangan'][-1]['SEMBUH'])
-    inTreatment = str(contents['list_perkembangan'][-1]['DIRAWAT_OR_ISOLASI'])
-    data.extend((case, dead, cured, inTreatment))
+    contents = requests.get('https://corona.jatengprov.go.id/data/chart_harian_akumulatif').json()
+    last_update = datetime.strptime(contents[-1]['tanggal'], "%Y-%m-%d")
+    converted_date = last_update.strftime("%d-%m-%Y")
+    positif = contents[-1]['covid']
+    sembuh = contents[-1]['sembuh']
+    meninggal = contents[-1]['dead']
+    total_covid = contents[-1]['covid_sum']
+    total_sembuh = contents[-1]['sembuh_sum']
+    total_meninggal = contents[-1]['dead_sum']
+    total_positif = int(total_covid) - int(total_meninggal) - int(total_sembuh)
+    data.extend((converted_date, positif, sembuh, meninggal, total_covid, str(total_positif), total_meninggal, total_sembuh))
     return data
-
-def getAccumulation():
-    dataAkumulasi = []
-    contents = requests.get('https://data.covid19.go.id/public/api/prov_detail_JAWA_TENGAH.json').json()
-    caseAcm = str(contents['list_perkembangan'][-1]['AKUMULASI_KASUS'])
-    deadAcm = str(contents['list_perkembangan'][-1]['AKUMULASI_MENINGGAL'])
-    curedAcm = str(contents['list_perkembangan'][-1]['AKUMULASI_SEMBUH'])
-    inTreatmentAcm = str(contents['list_perkembangan'][-1]['AKUMULASI_DIRAWAT_OR_ISOLASI'])
-    deadPct = str("%.2f" % contents['meninggal_persen'])
-    curedPct = str("%.2f" % contents['sembuh_persen'])
-    dataAkumulasi.extend((caseAcm, deadAcm, curedAcm, inTreatmentAcm, deadPct, curedPct))
-    return dataAkumulasi
 
 def start(bot, update):
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text='Halo! Berikut daftar perintah untuk menggunakan bot ini.\n/terbaru untuk melihat informasi terbaru COVID-19 di Jawa Tengah\n/akumulasi untuk melihat akumulasi kasus COVID-19 di Jawa Tengah sampai saat ini')
 
 def terbaru(bot, update):
-    data = getLastUpdate()
+    data = getData()
     chat_id = update.message.chat_id
-    isi_teks = '*Situasi COVID-19 di Jawa Tengah pada ' + data[0] + '*' + '\nKasus: *' + data[1] + '*\nMeninggal: *' + data[2] + '*\nSembuh: *' + data[3] + '*\nDirawat (isolasi): *' + data[4] + '*'
+    isi_teks = '*Situasi COVID-19 di Jawa Tengah pada ' + data[0] + '*' + '\nPositif COVID: *' + data[1] + '*\nSembuh: *' + data[2] + '*\nMeninggal: *' + data[3] + '*'
     bot.send_message(chat_id=chat_id, text=isi_teks, parse_mode="Markdown")
 
 def akumulasi(bot, update):
-    data = getAccumulation()
+    data = getData()
     chat_id = update.message.chat_id
-    isi_teks = '*Akumulasi Kasus COVID-19 di Jawa Tengah*' + '\nKasus: *' + data[0] + '*\nMeninggal: *' + data[1] + '*\nSembuh: *' + data[2] + '*\nDirawat (isolasi): *' + data[3] + '*\nPersentase Meninggal: *' + data[4] +'%*\nPersentase Sembuh: *' + data[5] + '%*'
+    isi_teks = '*Akumulasi Kasus COVID-19 di Jawa Tengah*' + '\nJumlah kasus: *' + data[4] + '*\nPositif: *' + data[5] + '*\nSembuh: *' + data[6] + '*\nMeninggal: *' + data[7] + '*'
     bot.send_message(chat_id=chat_id, text=isi_teks, parse_mode="Markdown")
 
 def main():
